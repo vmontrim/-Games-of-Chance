e7a9fcc2ccd6eb6cf561e4c7e35c3ac2c301a441
vmontrim <56654428+vmontrim@users.noreply.github.com>
2020-06-16 15:44:59 +0100

    Update games.py

--- games.py
+++ games.py
@@ -2,11 +2,23 @@ import random
 
 money = 100
 
-#Write your game of chance functions here
+def coin_flip(call, amount):
+  
+  if random.randint(1,2) == 1:
+    coin_toss = "Heads"
+  else:
+    coin_toss = "Tails"
+  if call == coin_toss:
+    result = "won"
+    total_amount = money + amount
+  else:
+    result = "lost"
+    total_amount = money - amount
+  
+  print(
+    "You flipped a %s and you called %s! You %s and now have $%.2f"
+    %(coin_toss, call, result, total_amount)
+  )
+  
+coin_flip("Heads", 10)
 
-
-
-
-
-
-#Call your game of chance functions here
