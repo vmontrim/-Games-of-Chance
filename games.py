0e35fa261be0e6771bfb1689858b9eb22aca61d9
vmontrim <56654428+vmontrim@users.noreply.github.com>
2020-06-20 15:56:07 +0100

    Update games.py

--- games.py
+++ games.py
@@ -16,9 +16,7 @@ def coin_flip(call, amount):
     total_amount = money - amount
   
   print(
-    "You flipped a %s and you called %s! You %s and now have $%.2f"
-    %(coin_toss, call, result, total_amount)
+    f"You flipped {coin_toss} and you called {call}! You {result} and now have {total_amount}"
   )
   
 coin_flip("Heads", 10)
-
